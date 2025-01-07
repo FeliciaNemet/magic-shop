e84ff14f6653a705a78e3f0390197a64eb1a0264
FeliciaNemet <nemet.felicia@gmail.com>
2025-01-06 22:39:42 -0500

    Added basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
    <>
-   <Header />
+   <BrowserRouter>
+    <Header /> 
+    <Routes>
+
+
+    </Routes>
+    <Footer />
+   </BrowserRouter>
    </>
   );
 }
