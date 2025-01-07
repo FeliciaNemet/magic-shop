1143bd4622571ecfa4227f58a022b475fdddeebd
FeliciaNemet <nemet.felicia@gmail.com>
2025-01-06 22:32:32 -0500

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.scss';
+
+const Header = () => {
+
+
+
+    return (
+        <>
+        
+        <h1>Boo</h1>
+        </>
+    )
+}
+
+export default Header;
